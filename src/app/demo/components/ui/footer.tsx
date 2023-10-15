38614b5078715296b130ab45c381179ef1a27a4f
John Polacek <johnpolacek@gmail.com>
2023-10-15 13:35:28 -0500

    Update footer layout and styling

--- src/app/demo/components/ui/footer.tsx
+++ src/app/demo/components/ui/footer.tsx
@@ -5,7 +5,7 @@ import GithubLink from "./github-link"
 const Footer = () => {
   return (
     <footer className="pt-8 pb-12 sm:px-8 border-t bg-gradient-to-b from-gray-50 via-white opacity-70 to-white">
-      <div className="max-w-[1024px] px-4 mx-auto flex justify-between">
+      <div className="max-w-[1024px] px-4 mx-auto flex flex-col sm:flex-row justify-between">
         <div className="flex flex-col items-center sm:flex-row gap-2 sm:gap-8">
           <p className="text-gray-600 whitespace-nowrap">© {new Date().getFullYear()} Bucket CMS</p>
           <Link className="text-blue-600" href="/privacy">
@@ -18,7 +18,7 @@ const Footer = () => {
             License
           </Link>
         </div>
-        <div className="relative sm:left-16">
+        <div className="relative sm:left-16 flex justify-center py-6 pl-4 sm:block sm:p-0">
           <GithubLink />
         </div>
       </div>
