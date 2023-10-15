8d6e09d041678cd786ea9790a71a853bebdb5eab
John Polacek <johnpolacek@gmail.com>
2023-10-15 16:34:32 -0500

    Update metadata in layout.tsx for Bucket CMS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,40 @@ import Footer from "./demo/components/ui/footer"
 
 import type { Metadata } from "next"
 
+const title = "Bucket CMS"
+const description = "Bucket CMS is the world’s first AI-Powered Portable Drop-in Headless CMS (no database necessary)."
+const url = "https://bucket-cms.com"
+const screenshot = "https://bucket-cms.com/screenshot.jpg"
+
 export const metadata: Metadata = {
-  title: "Bucket CMS",
-  description: "Bucket is the world’s first AI-Powered Portable Drop-in Headless CMS (no database necessary).",
+  title,
+  description,
   icons: {
     icon: "/favicon-32x32.png",
   },
+  openGraph: {
+    title: "Bucket CMS",
+    description: "Bucket CMS is the world’s first AI-Powered Portable Drop-in Headless CMS (no database necessary).",
+    siteName: "Bucket CMS",
+    images: [screenshot],
+    url,
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "bucket_cms",
+    images: [
+      {
+        url: screenshot,
+        alt: "Bucket CMS Website, Install with npx create-bucket-cms",
+        type: "image/jpeg",
+        width: 1259,
+        height: 710,
+      },
+    ],
+  },
+  keywords: ["headless cms", "cms", "content management system", "next.js cms", "react cms", "drop-in cms", "portable cms", "ai cms", "ai-powered cms"],
+  creator: "John Polacek",
+  publisher: "Bucket CMS",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
